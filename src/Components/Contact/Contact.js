edebc55b0b8a940feb28ac8597a453c54e15d8e7
ngmnhdc <ngmnhdc@gmail.com>
2022-01-23 19:32:39 +0700

    #7: Add input validator

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,12 +6,62 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import emailjs from '@emailjs/browser';
 import './Contact.css'
+import Validator from '../../validator';
 
 function Contact() {
     const form = useRef();
 
+    this.state = {
+        name: '',
+        email: '',
+        message: '',
+        errors: {},
+    };
+
+    const { errors } = this.state;
+
+    // const requiredWith = (value, field, state) => (!state[field] && !value) || !!value;
+    const rules = [
+        {
+            field: 'name',
+            method: 'isEmpty',
+            validWhen: false,
+            message: 'The name field is required.',
+        },
+        {
+            field: 'email',
+            method: 'isEmpty',
+            validWhen: false,
+            message: 'The email field is required.',
+        },
+        {
+            field: 'email',
+            method: 'isEmail',
+            validWhen: true,
+            message: 'Invalid email address.',
+        },
+        {
+            field: 'message',
+            method: 'isEmpty',
+            validWhen: false,
+            message: 'Please fill out this field.',
+        },
+    ];
+    this.validator = new Validator(rules);
+
+    function handleInput(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
     function handleSubmit(e) {
         e.preventDefault();
+
+        this.setState({
+            errors: this.validator.validate(this.state),
+        });
+        console.log(this.state);
 
         emailjs.sendForm(
             'service_i5q09zf',
@@ -24,6 +74,7 @@ function Contact() {
             console.log(error.text);
         });
     };
+
     return (
         <div id="contact" className="contact-wrapper">
             <div className="contact-info" data-aos="fade-right" data-aos-duration="1000">
@@ -41,15 +92,37 @@ function Contact() {
                     <p className="contact-text">Thu Duc City, Ho Chi Minh City, Vietnam</p>
                 </div>
             </div>
-            <div className="contact-form">
+            <div className="contact-form" id="contact-form">
                 {/* <p className="contact-desc">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p> */ }
                 <form className="contact-form-message" ref={ form } data-aos="fade-left" data-aos-duration="1000">
-                    <label className="form-item" htmlFor="name">Name</label>
-                    <input className="form-input" type="text" name="name" placeholder="Your name" required />
-                    <label className="form-item" htmlFor="email">Email</label>
-                    <input className="form-input" type="text" name="email" placeholder="Your email" required />
-                    <label className="form-item" htmlFor="message">Message</label>
-                    <textarea className="form-input" type="text" name="message" placeholder="Leave a message" rows="4" required />
+                    <label className="form-item" id="name" htmlFor="name">Name</label>
+                    <input
+                        className="form-input"
+                        type="text"
+                        name="name"
+                        placeholder="Your name" required
+                        value={ this.state.name } onChange={ handleInput } />
+                    { errors.name && <div className="form-error" style={ { display: 'block' } }>{ errors.name }</div> }
+                    <label className="form-item" id="email" htmlFor="email">Email</label>
+                    <input
+                        className="form-input"
+                        type="text"
+                        name="email"
+                        placeholder="Your email" required
+                        value={ this.state.email } onChange={ handleInput }
+                    />
+                    { errors.name && <div className="form-error" style={ { display: 'block' } }>{ errors.name }</div> }
+                    <label className="form-item" id="message" htmlFor="message">Message</label>
+                    <textarea
+                        className="form-input"
+                        type="text"
+                        name="message"
+                        placeholder="Leave a message"
+                        rows="4" required
+                        value={ this.state.message }
+                        onChange={ handleInput }
+                    />
+                    { errors.name && <div className="form-error" style={ { display: 'block' } }>{ errors.name }</div> }
                     <Stack className="contact-btns" direction="row" spacing={ 1 }>
                         <Button
                             className="btn btn-send"
@@ -65,5 +138,4 @@ function Contact() {
     )
 }
 
-export default Contact
-    ;
+export default Contact;
